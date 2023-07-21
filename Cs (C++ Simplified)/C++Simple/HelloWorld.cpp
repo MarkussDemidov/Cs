70cceb86402dff7d27f61a774b57184059416ec4
Markuss Demidovs <136814349+MarkussDemidov@users.noreply.github.com>
2023-07-21 23:10:40 +0300

    Version 1.0

---
+++ Cs (C++ Simplified)/C++Simple/HelloWorld.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// IGNORE THIS!!
+ // std:: (standart) remover
+ using namespace std;
+ // typedef
+ typedef string text;
+ typedef int number;
+ typedef double fractional;
+ typedef bool turner;
+ auto& output = std::cout;
+ auto& input = std::cin;
+ void test() {
+	text textTest = "Hello";
+	textTest.append(" World!");
+	number numTest = 1;
+	numTest += 1;
+	fractional fracTest = 1.3;
+	turner turnTest = true;
+	cout << "Hello world!" << endl;
+	cout << textTest <<  '\n';
+	cout << numTest << '\n';
+	cout << fracTest << '\n';
+	cout << turnTest << '\n';
+ }
+
+
+// WRITE YOUR CODE HERE!!
+// if you want to test if everything is working correctly, type: test();
+// Instructions are listed in a seperate file.
+int main() {
+
+	return 0;
+}
